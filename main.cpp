e8c497b39447766bdbc68a21de48515d52f4211e
Phoenix136 <antoinechevalier@me.com>
2021-09-22 21:05:13 -0400

    optmization

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 #define CHEVAN_UTILS_INCL
 #define CHEVAN_UTILS_PRINT
 #define CHEVAN_UTILS_bytes
-#include <boost/multiprecision/cpp_int.hpp>
 #include <gmp.h>
+#include <boost/multiprecision/cpp_int.hpp>
 typedef __uint128_t bigUInt;
 typedef boost::multiprecision::uint1024_t biggerUInt;
 namespace chevan_utils
@@ -17,7 +17,7 @@ namespace chevan_utils
   {
     if (n < UINT64_MAX)
     {
-      std::cout << (uint64_t)n << std::endl;
+      std::cout << (uint64_t)n;
       return;
     }
     char str[40] = {0};
@@ -33,11 +33,6 @@ namespace chevan_utils
   }
   static void print(biggerUInt n)
   {
-    if (n < UINT64_MAX)
-    {
-      std::cout << (uint64_t)n << std::endl;
-      return;
-    }
     if (n < (biggerUInt)std::pow(2, 128))
     {
       print((bigUInt)n);
@@ -56,20 +51,17 @@ namespace chevan_utils
   }
 }
 #include "utils.hpp"
-namespace chevan_utils
-{
-  template <typename... P>
-  static void atomic_println(P... p)
-  {
-    mutex.lock();
-    println(p...);
-    mutex.unlock();
-  }
-}
 using namespace chevan_utils;
-#define print atomic_println
+template <typename... P>
+static void atomic_println(P... p)
+{
+  mutex.lock();
+  println(p...);
+  mutex.unlock();
+}
+#define print atomic_println // whenever printing, use this function instead
 
-#define THREAD_COUNT 32
+#define THREAD_COUNT 32 // number of threads to use
 #define MAX_K_TESTED 120
 
 template <typename N>
@@ -88,6 +80,8 @@ static bool isPrime(N n)
 template <typename T>
 static bool isMersennesPrime(T m, T p)
 {
+  if (!isPrime(p))
+    return false;
   T s = 4;
   for (T i = 3; i <= p; i++)
   {
@@ -98,6 +92,8 @@ static bool isMersennesPrime(T m, T p)
 template <typename T>
 static bool isMersennesPrime(mpz_t m, T p)
 {
+  if (!isPrime(p))
+    return false;
   mpz_t s;
   mpz_init(s);
   mpz_set_ui(s, 4);
@@ -114,13 +110,8 @@ static bool isMersennesPrime(mpz_t m, T p)
 template <typename T>
 static void euclid_euler(T k)
 {
-  T power = 1;
-  // std::pow is borked and returns infinity
-  for (u64 p = 0; p < k - 1; p++)
-  {
-    power *= 2;
-  }
-  T primePart = power * 2 - 1;
+  T power = ((T)1) << (k - 1);    // 2 ^ (k-1)
+  T primePart = (power << 1) - 1; // power * 2 - 1 or 2 ^ k - 1
 
   if (k > 2)
   {
@@ -142,16 +133,46 @@ static void euclid_euler(T k)
     print(power * primePart);
   }
 }
+// 1<<(k-1) is faster but biggerUInt doesn't support it
+static void euclid_euler(biggerUInt k)
+{
+  biggerUInt power = 1;
+  // std::pow is borked and returns infinity
+  for (u64 p = 0; p < k - 1; p++)
+  {
+    power *= 2;
+  }
+  biggerUInt primePart = power * 2 - 1;
 
+  if (k > 2)
+  {
+    if (!isMersennesPrime(primePart, k))
+      return;
+  }
+  else
+  {
+    if (!isPrime(primePart))
+      return;
+  }
+
+  if (k >= MAX_K_TESTED)
+  {
+    print("imprecise: ", power * primePart);
+  }
+  else
+  {
+    print(power * primePart);
+  }
+}
 static void euclid_euler_gmp(biggerUInt k)
 {
   mpz_t power;
   mpz_init(power);
-  mpz_set_ui(power, 1);
-  for (u64 p = 0; p < k - 1; p++)
-  {
-    mpz_mul_ui(power, power, 2);
-  }
+  // mpz_set_ui(power,1);
+  // mpn_lshift(power->_mp_d, power->_mp_d, power->_mp_alloc, (u64)(k - 1));
+  mpz_set_ui(power, 2);
+  mpz_pow_ui(power, power, (u64)k - 1);
+
   mpz_t primePart;
   mpz_init(primePart);
   mpz_mul_ui(primePart, power, 2);
@@ -169,26 +190,29 @@ static void euclid_euler_gmp(biggerUInt k)
 
 static void threadLaunch(uchar t)
 {
-  for (bigUInt k = t + 2; 1; k += THREAD_COUNT)
+  if(t==0)
   {
-    if (k < 17)
+    euclid_euler<u32>(2);
+  }
+  for (bigUInt k = t * 2 + 3; 1; k += THREAD_COUNT * 2)
+  {
+    if (k >= MAX_K_TESTED) // tested first
+    {
+      euclid_euler_gmp(k);
+    }
+    else if (k < 17)
       euclid_euler<u32>(k);
     else if (k < 33)
       euclid_euler<u64>(k);
     else if (k < 65)
       euclid_euler<bigUInt>(k);
     else if (k < MAX_K_TESTED)
-      euclid_euler<biggerUInt>(k);
-    else
-    {
-      euclid_euler_gmp(k);
-    }
+      euclid_euler((biggerUInt)k);
   }
 }
 
 int main()
 {
-  // return 0;
   std::vector<std::thread> threads = std::vector<std::thread>(THREAD_COUNT);
   if (THREAD_COUNT == 1)
   {
